806f0ae00e65f34b33cb583ebe0d7fab51cf482e
unknown <shkim5276@uos.ac.kr>
2018-05-30 09:47:16 +0900

    search movie

---
+++ DB_Project_Cinema/MovieSearchList.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace DB_Project_Cinema
+{
+    partial class MovieSearchList
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.MovieNM = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DirectorNM = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Rating = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MovieNM,
+            this.DirectorNM,
+            this.Rating});
+            this.dataGridView1.Location = new System.Drawing.Point(378, 140);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 30;
+            this.dataGridView1.Size = new System.Drawing.Size(650, 104);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // MovieNM
+            // 
+            this.MovieNM.HeaderText = "영화명";
+            this.MovieNM.Name = "MovieNM";
+            this.MovieNM.ReadOnly = true;
+            // 
+            // DirectorNM
+            // 
+            this.DirectorNM.HeaderText = "감독명";
+            this.DirectorNM.Name = "DirectorNM";
+            this.DirectorNM.ReadOnly = true;
+            // 
+            // Rating
+            // 
+            this.Rating.HeaderText = "관람등급";
+            this.Rating.Name = "Rating";
+            this.Rating.ReadOnly = true;
+            // 
+            // MovieSearchList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "MovieSearchList";
+            this.Size = new System.Drawing.Size(1165, 501);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MovieNM;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DirectorNM;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Rating;
+    }
+}
---
+++ DB_Project_Cinema/MovieSearchList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+
+
+namespace DB_Project_Cinema
+{
+    public partial class MovieSearchList : UserControl
+    {
+        public MovieSearchList()
+        {
+            InitializeComponent();
+
+            
+
+            
+            string str = "data source=localhost:1521/xe;user id=CINEMA; password=1234";
+            OracleConnection Conn = new OracleConnection(str);
+            /**OracleCommand Comm;
+            Comm = new OracleCommand();
+            Comm.Connection = Conn;*/
+            try
+            {
+
+                Conn.Open();
+                string sql = "SELECT * FROM MOVIE WHERE MOVIE_NO = 3";
+
+                OracleCommand Comm = new OracleCommand(sql, Conn);
+
+
+                OracleDataReader reader = Comm.ExecuteReader();
+
+
+                while (reader.Read())
+                {
+                    string a = reader.GetString(reader.GetOrdinal("MOVIE_NM"));
+                    //this.Genre.Text = reader.GetString(reader.GetOrdinal("GENRE"));
+                    //this.ReleaseDate.Text = reader.GetDateTime(reader.GetOrdinal("RELEASE_DATE")).ToShortDateString();
+                    //this.ShowTime.Text = reader.GetString(reader.GetOrdinal("SHOW_TIME"));
+                    string c= reader.GetString(reader.GetOrdinal("RATING"));
+                    string b=reader.GetString(reader.GetOrdinal("DIRECTOR_NM"));
+                   // this.ActorNM.Text = reader.GetString(reader.GetOrdinal("ACTOR_NM"));
+                    //this.Country.Text = reader.GetString(reader.GetOrdinal("COUNTRY"));
+                    //this.Distributor.Text = reader.GetString(reader.GetOrdinal("DISTRIBUTOR"));
+                    //this.MovieIntro.Text = reader.GetString(reader.GetOrdinal("MOVIE_INTRO"));
+                    //var poster = reader.GetString(reader.GetOrdinal("POSTER"));
+
+                    //Poster.SizeMode = PictureBoxSizeMode.StretchImage;
+                    //Poster.ImageLocation = poster;
+
+                    string[] row0={a,b,c};
+
+                    dataGridView1.Rows.Add(row0);
+                }
+
+
+                
+                Conn.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+
+            }
+            finally
+            {
+                Conn.Close();
+            }
+        }
+        }
+    }
+
