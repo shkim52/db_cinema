768d1a9404540018ac9ffae84accbe344d334225
yuuun <ynkim31@naver.com>
2018-06-02 01:39:46 +0900

    quit stat change

--- DB_Project_Cinema/MyPageQuitMem.cs
+++ DB_Project_Cinema/MyPageQuitMem.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+
 
 namespace DB_Project_Cinema
 {
@@ -19,7 +22,31 @@ namespace DB_Project_Cinema
 
         private void QuitMemButton_Click(object sender, EventArgs e)
         {
+            string str = "data source=localhost:1521/xe;user id=CINEMA; password=1234";
+            OracleConnection Conn = new OracleConnection(str);
 
+            try
+            {
+                OracleCommand Cmd = new OracleCommand();
+                Cmd.Connection = Conn;
+                Conn.Open();
+
+                string input_sql = "UPDATE MEM SET QUIT_STAT = 'N'";
+                Cmd.CommandText = input_sql;
+                Cmd.ExecuteNonQuery();
+                MessageBox.Show("탈퇴가 완료되었습니다.");
+                
+
+                Conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Conn.Close();
+            }
         }
     }
 }
