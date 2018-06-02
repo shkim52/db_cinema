3ea842fb6cc80d8c6554ab58d0f7a3cb4a9ba5be
unknown <shkim5276@uos.ac.kr>
2018-06-02 18:12:30 +0900

    moviepage button remove

--- DB_Project_Cinema/MoviePage.Designer.cs
+++ DB_Project_Cinema/MoviePage.Designer.cs
@@ -33,20 +33,11 @@
             this.SearchText = new System.Windows.Forms.TextBox();
             this.MovieCategory = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.ReviewButton = new System.Windows.Forms.Button();
-            this.BackToHomeButton = new System.Windows.Forms.Button();
-            this.Movie5DetailButton = new System.Windows.Forms.Button();
-            this.Movie4DetailButton = new System.Windows.Forms.Button();
-            this.Movie3DetailButton = new System.Windows.Forms.Button();
-            this.Movie2DetailButton = new System.Windows.Forms.Button();
-            this.Movie1DetailButton = new System.Windows.Forms.Button();
             this.MovieSearchPage = new DB_Project_Cinema.MovieSearchPage();
             this.MovieDetail = new DB_Project_Cinema.MovieDetail1();
             this.ReviewPage = new DB_Project_Cinema.RevieiwPage();
             this.MovieList = new DB_Project_Cinema.MovieList();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -111,127 +102,6 @@
             this.label1.Text = "Movie";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.ReviewButton);
-            this.panel2.Controls.Add(this.BackToHomeButton);
-            this.panel2.Controls.Add(this.Movie5DetailButton);
-            this.panel2.Controls.Add(this.Movie4DetailButton);
-            this.panel2.Controls.Add(this.Movie3DetailButton);
-            this.panel2.Controls.Add(this.Movie2DetailButton);
-            this.panel2.Controls.Add(this.Movie1DetailButton);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 495);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1702, 95);
-            this.panel2.TabIndex = 1;
-            // 
-            // ReviewButton
-            // 
-            this.ReviewButton.BackColor = System.Drawing.Color.LightGray;
-            this.ReviewButton.FlatAppearance.BorderSize = 0;
-            this.ReviewButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ReviewButton.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.ReviewButton.Location = new System.Drawing.Point(606, 26);
-            this.ReviewButton.Margin = new System.Windows.Forms.Padding(2);
-            this.ReviewButton.Name = "ReviewButton";
-            this.ReviewButton.Size = new System.Drawing.Size(172, 36);
-            this.ReviewButton.TabIndex = 17;
-            this.ReviewButton.Text = "리뷰 보기/등록";
-            this.ReviewButton.UseVisualStyleBackColor = false;
-            this.ReviewButton.Click += new System.EventHandler(this.ReviewButton_Click_2);
-            // 
-            // BackToHomeButton
-            // 
-            this.BackToHomeButton.BackColor = System.Drawing.Color.LightGray;
-            this.BackToHomeButton.FlatAppearance.BorderSize = 0;
-            this.BackToHomeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BackToHomeButton.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.BackToHomeButton.Location = new System.Drawing.Point(314, 26);
-            this.BackToHomeButton.Margin = new System.Windows.Forms.Padding(2);
-            this.BackToHomeButton.Name = "BackToHomeButton";
-            this.BackToHomeButton.Size = new System.Drawing.Size(162, 36);
-            this.BackToHomeButton.TabIndex = 13;
-            this.BackToHomeButton.Text = "<-홈으로";
-            this.BackToHomeButton.UseVisualStyleBackColor = false;
-            this.BackToHomeButton.Click += new System.EventHandler(this.BackToHomeButton_Click_2);
-            // 
-            // Movie5DetailButton
-            // 
-            this.Movie5DetailButton.BackColor = System.Drawing.Color.Wheat;
-            this.Movie5DetailButton.FlatAppearance.BorderSize = 0;
-            this.Movie5DetailButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Movie5DetailButton.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.Movie5DetailButton.Location = new System.Drawing.Point(1144, 26);
-            this.Movie5DetailButton.Margin = new System.Windows.Forms.Padding(2);
-            this.Movie5DetailButton.Name = "Movie5DetailButton";
-            this.Movie5DetailButton.Size = new System.Drawing.Size(202, 36);
-            this.Movie5DetailButton.TabIndex = 14;
-            this.Movie5DetailButton.Text = "영화이름";
-            this.Movie5DetailButton.UseVisualStyleBackColor = false;
-            this.Movie5DetailButton.Click += new System.EventHandler(this.Movie5DetailButton_Click_1);
-            // 
-            // Movie4DetailButton
-            // 
-            this.Movie4DetailButton.BackColor = System.Drawing.Color.Wheat;
-            this.Movie4DetailButton.FlatAppearance.BorderSize = 0;
-            this.Movie4DetailButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Movie4DetailButton.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.Movie4DetailButton.Location = new System.Drawing.Point(898, 26);
-            this.Movie4DetailButton.Margin = new System.Windows.Forms.Padding(2);
-            this.Movie4DetailButton.Name = "Movie4DetailButton";
-            this.Movie4DetailButton.Size = new System.Drawing.Size(202, 36);
-            this.Movie4DetailButton.TabIndex = 16;
-            this.Movie4DetailButton.Text = "영화이름";
-            this.Movie4DetailButton.UseVisualStyleBackColor = false;
-            this.Movie4DetailButton.Click += new System.EventHandler(this.Movie4DetailButton_Click_1);
-            // 
-            // Movie3DetailButton
-            // 
-            this.Movie3DetailButton.BackColor = System.Drawing.Color.Wheat;
-            this.Movie3DetailButton.FlatAppearance.BorderSize = 0;
-            this.Movie3DetailButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Movie3DetailButton.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.Movie3DetailButton.Location = new System.Drawing.Point(639, 26);
-            this.Movie3DetailButton.Margin = new System.Windows.Forms.Padding(2);
-            this.Movie3DetailButton.Name = "Movie3DetailButton";
-            this.Movie3DetailButton.Size = new System.Drawing.Size(202, 36);
-            this.Movie3DetailButton.TabIndex = 15;
-            this.Movie3DetailButton.Text = "영화이름";
-            this.Movie3DetailButton.UseVisualStyleBackColor = false;
-            this.Movie3DetailButton.Click += new System.EventHandler(this.Movie3DetailButton_Click_1);
-            // 
-            // Movie2DetailButton
-            // 
-            this.Movie2DetailButton.BackColor = System.Drawing.Color.Wheat;
-            this.Movie2DetailButton.FlatAppearance.BorderSize = 0;
-            this.Movie2DetailButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Movie2DetailButton.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.Movie2DetailButton.Location = new System.Drawing.Point(372, 26);
-            this.Movie2DetailButton.Margin = new System.Windows.Forms.Padding(2);
-            this.Movie2DetailButton.Name = "Movie2DetailButton";
-            this.Movie2DetailButton.Size = new System.Drawing.Size(202, 36);
-            this.Movie2DetailButton.TabIndex = 14;
-            this.Movie2DetailButton.Text = "영화이름";
-            this.Movie2DetailButton.UseVisualStyleBackColor = false;
-            this.Movie2DetailButton.Click += new System.EventHandler(this.Movie2DetailButton_Click_1);
-            // 
-            // Movie1DetailButton
-            // 
-            this.Movie1DetailButton.BackColor = System.Drawing.Color.Wheat;
-            this.Movie1DetailButton.FlatAppearance.BorderSize = 0;
-            this.Movie1DetailButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Movie1DetailButton.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.Movie1DetailButton.Location = new System.Drawing.Point(106, 26);
-            this.Movie1DetailButton.Margin = new System.Windows.Forms.Padding(2);
-            this.Movie1DetailButton.Name = "Movie1DetailButton";
-            this.Movie1DetailButton.Size = new System.Drawing.Size(202, 36);
-            this.Movie1DetailButton.TabIndex = 13;
-            this.Movie1DetailButton.Text = "영화이름";
-            this.Movie1DetailButton.UseVisualStyleBackColor = false;
-            this.Movie1DetailButton.Click += new System.EventHandler(this.Movie1DetailButton_Click_2);
-            // 
             // MovieSearchPage
             // 
             this.MovieSearchPage.BackColor = System.Drawing.Color.White;
@@ -241,7 +111,7 @@
             this.MovieSearchPage.Location = new System.Drawing.Point(0, 188);
             this.MovieSearchPage.Margin = new System.Windows.Forms.Padding(2);
             this.MovieSearchPage.Name = "MovieSearchPage";
-            this.MovieSearchPage.Size = new System.Drawing.Size(1702, 307);
+            this.MovieSearchPage.Size = new System.Drawing.Size(1702, 402);
             this.MovieSearchPage.TabIndex = 5;
             // 
             // MovieDetail
@@ -251,7 +121,7 @@
             this.MovieDetail.Location = new System.Drawing.Point(0, 188);
             this.MovieDetail.Margin = new System.Windows.Forms.Padding(2);
             this.MovieDetail.Name = "MovieDetail";
-            this.MovieDetail.Size = new System.Drawing.Size(1702, 307);
+            this.MovieDetail.Size = new System.Drawing.Size(1702, 402);
             this.MovieDetail.TabIndex = 4;
             // 
             // ReviewPage
@@ -261,7 +131,7 @@
             this.ReviewPage.Location = new System.Drawing.Point(0, 188);
             this.ReviewPage.Margin = new System.Windows.Forms.Padding(2);
             this.ReviewPage.Name = "ReviewPage";
-            this.ReviewPage.Size = new System.Drawing.Size(1702, 307);
+            this.ReviewPage.Size = new System.Drawing.Size(1702, 402);
             this.ReviewPage.TabIndex = 3;
             // 
             // MovieList
@@ -272,7 +142,7 @@
             this.MovieList.Location = new System.Drawing.Point(0, 188);
             this.MovieList.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.MovieList.Name = "MovieList";
-            this.MovieList.Size = new System.Drawing.Size(1702, 307);
+            this.MovieList.Size = new System.Drawing.Size(1702, 402);
             this.MovieList.TabIndex = 2;
             // 
             // MoviePage
@@ -284,14 +154,12 @@
             this.Controls.Add(this.MovieDetail);
             this.Controls.Add(this.ReviewPage);
             this.Controls.Add(this.MovieList);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MoviePage";
             this.Size = new System.Drawing.Size(1702, 590);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -303,14 +171,6 @@
         private System.Windows.Forms.TextBox SearchText;
         private System.Windows.Forms.ListBox MovieCategory;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button ReviewButton;
-        private System.Windows.Forms.Button BackToHomeButton;
-        private System.Windows.Forms.Button Movie5DetailButton;
-        private System.Windows.Forms.Button Movie4DetailButton;
-        private System.Windows.Forms.Button Movie3DetailButton;
-        private System.Windows.Forms.Button Movie2DetailButton;
-        private System.Windows.Forms.Button Movie1DetailButton;
         private MovieList MovieList;
         private RevieiwPage ReviewPage;
         private MovieDetail1 MovieDetail;
--- DB_Project_Cinema/MoviePage.cs
+++ DB_Project_Cinema/MoviePage.cs
@@ -26,211 +26,21 @@ namespace DB_Project_Cinema
                 return _instance;
             }
         }
+
+        
+
         public MoviePage()
         {
             InitializeComponent();
 
-            string str = "data source=localhost:1521/xe;user id=CINEMA; password=1234";
-            OracleConnection Conn = new OracleConnection(str);
-            /**OracleCommand Comm;
-            Comm = new OracleCommand();
-            Comm.Connection = Conn;*/
-            try
-            {
-
-                Conn.Open();
-
-                string sql1 = "SELECT * FROM MOVIE WHERE MOVIE_NO=1 ";
-                OracleCommand Comm1 = new OracleCommand(sql1, Conn);
-
-                OracleDataReader reader1 = Comm1.ExecuteReader();
-
-                while (reader1.Read())
-                {
-                    this.Movie1DetailButton.Text = reader1.GetString(reader1.GetOrdinal("MOVIE_NM"));
-
-                }
-
-                string sql2 = "SELECT * FROM MOVIE WHERE MOVIE_NO=2 ";
-                OracleCommand Comm2 = new OracleCommand(sql2, Conn);
-
-                OracleDataReader reader2 = Comm2.ExecuteReader();
-
-                while (reader2.Read())
-                {
-                    this.Movie2DetailButton.Text = reader2.GetString(reader2.GetOrdinal("MOVIE_NM"));
-                }
-
-                string sql3 = "SELECT * FROM MOVIE WHERE MOVIE_NO=3 ";
-                OracleCommand Comm3 = new OracleCommand(sql3, Conn);
-
-                OracleDataReader reader3 = Comm3.ExecuteReader();
-
-                while (reader3.Read())
-                {
-                    this.Movie3DetailButton.Text = reader3.GetString(reader3.GetOrdinal("MOVIE_NM"));
-                }
-
-                string sql4 = "SELECT * FROM MOVIE WHERE MOVIE_NO=4 ";
-                OracleCommand Comm4 = new OracleCommand(sql4, Conn);
-
-                OracleDataReader reader4 = Comm4.ExecuteReader();
-
-                while (reader4.Read())
-                {
-                    this.Movie4DetailButton.Text = reader4.GetString(reader4.GetOrdinal("MOVIE_NM"));
-                }
-
-                string sql5 = "SELECT * FROM MOVIE WHERE MOVIE_NO=5 ";
-                OracleCommand Comm5 = new OracleCommand(sql5, Conn);
-
-                OracleDataReader reader5 = Comm5.ExecuteReader();
-
-                while (reader5.Read())
-                {
-                    this.Movie5DetailButton.Text = reader5.GetString(reader5.GetOrdinal("MOVIE_NM"));
-                }
-
-
-                Conn.Close();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-
-            }
-            finally
-            {
-                Conn.Close();
-            }
-
-
             MovieList.BringToFront();
+           
+ 
+        }      
+            
+            
+        
 
-            BackToHomeButton.Visible = false;
-            ReviewButton.Visible = false;
-        }
-
-
-
-        private void Movie1DetailButton_Click_2(object sender, EventArgs e)
-        {
-            Controls.Add(MovieDetail1.Instance);
-            MovieDetail1.Instance.setMovie_nm(Movie1DetailButton.Text);
-            MovieDetail1.Instance.MovieDetail_test();
-            MovieDetail1.Instance.Dock = DockStyle.Fill;
-            MovieDetail1.Instance.BringToFront();
-
-            Movie1DetailButton.Visible = false;
-            Movie2DetailButton.Visible = false;
-            Movie3DetailButton.Visible = false;
-            Movie4DetailButton.Visible = false;
-            Movie5DetailButton.Visible = false;
-
-            BackToHomeButton.Visible = true;
-            ReviewButton.Visible = true;               
-        }
-
-        private void Movie2DetailButton_Click_1(object sender, EventArgs e)
-        {
-            Controls.Add(MovieDetail1.Instance);
-            MovieDetail1.Instance.setMovie_nm(Movie2DetailButton.Text);
-            MovieDetail1.Instance.MovieDetail_test();
-            MovieDetail1.Instance.Dock = DockStyle.Fill;
-            MovieDetail1.Instance.BringToFront();
-
-            Movie1DetailButton.Visible = false;
-            Movie2DetailButton.Visible = false;
-            Movie3DetailButton.Visible = false;
-            Movie4DetailButton.Visible = false;
-            Movie5DetailButton.Visible = false;
-
-            BackToHomeButton.Visible = true;
-            ReviewButton.Visible = true;
-        }
-
-        private void Movie3DetailButton_Click_1(object sender, EventArgs e)
-        {
-            Controls.Add(MovieDetail1.Instance);
-            MovieDetail1.Instance.setMovie_nm(Movie3DetailButton.Text);
-            MovieDetail1.Instance.MovieDetail_test();
-            MovieDetail1.Instance.Dock = DockStyle.Fill;
-            MovieDetail1.Instance.BringToFront();
-
-            Movie1DetailButton.Visible = false;
-            Movie2DetailButton.Visible = false;
-            Movie3DetailButton.Visible = false;
-            Movie4DetailButton.Visible = false;
-            Movie5DetailButton.Visible = false;
-
-            BackToHomeButton.Visible = true;
-            ReviewButton.Visible = true;
-        }
-
-        private void Movie4DetailButton_Click_1(object sender, EventArgs e)
-        {
-            Controls.Add(MovieDetail1.Instance);
-            MovieDetail1.Instance.setMovie_nm(Movie4DetailButton.Text);
-            MovieDetail1.Instance.MovieDetail_test();
-            MovieDetail1.Instance.Dock = DockStyle.Fill;
-            MovieDetail1.Instance.BringToFront();
-
-            Movie1DetailButton.Visible = false;
-            Movie2DetailButton.Visible = false;
-            Movie3DetailButton.Visible = false;
-            Movie4DetailButton.Visible = false;
-            Movie5DetailButton.Visible = false;
-
-            BackToHomeButton.Visible = true;
-            ReviewButton.Visible = true;
-
-        }
-
-        private void Movie5DetailButton_Click_1(object sender, EventArgs e)
-        {
-            Controls.Add(MovieDetail1.Instance);
-            MovieDetail1.Instance.setMovie_nm(Movie5DetailButton.Text);
-            MovieDetail1.Instance.MovieDetail_test();
-            MovieDetail1.Instance.Dock = DockStyle.Fill;
-            MovieDetail1.Instance.BringToFront();
-
-            Movie1DetailButton.Visible = false;
-            Movie2DetailButton.Visible = false;
-            Movie3DetailButton.Visible = false;
-            Movie4DetailButton.Visible = false;
-            Movie5DetailButton.Visible = false;
-
-            BackToHomeButton.Visible = true;
-            ReviewButton.Visible = true;
-        }
-
-        private void BackToHomeButton_Click_2(object sender, EventArgs e)
-        {
-            MovieList.BringToFront();
-
-            Movie1DetailButton.Visible = true;
-            Movie2DetailButton.Visible = true;
-            Movie3DetailButton.Visible = true;
-            Movie4DetailButton.Visible = true;
-            Movie5DetailButton.Visible = true;
-
-            BackToHomeButton.Visible = false;
-            ReviewButton.Visible = false;
-        }
-
-        private void ReviewButton_Click_2(object sender, EventArgs e)
-        {
-            Movie1DetailButton.Visible = false;
-            Movie2DetailButton.Visible = false;
-            Movie3DetailButton.Visible = false;
-            Movie4DetailButton.Visible = false;
-            Movie5DetailButton.Visible = false;
-
-            BackToHomeButton.Visible = true;
-            ReviewButton.Visible = false;
-
-            ReviewPage.BringToFront();
-        }
 
         private void SearchButton_Click_1(object sender, EventArgs e)
         {
@@ -255,14 +65,7 @@ namespace DB_Project_Cinema
                         OracleDataReader reader2 = Comm.ExecuteReader();
                         if (reader2.HasRows)
                         {
-                            Movie1DetailButton.Visible = false;
-                            Movie2DetailButton.Visible = false;
-                            Movie3DetailButton.Visible = false;
-                            Movie4DetailButton.Visible = false;
-                            Movie5DetailButton.Visible = false;
-
-                            BackToHomeButton.Visible = true;
-                            ReviewButton.Visible = false;
+                            
 
                             MovieSearchPage.BringToFront();
 
@@ -285,14 +88,7 @@ namespace DB_Project_Cinema
                         OracleDataReader reader2 = Comm.ExecuteReader();
                         if (reader2.HasRows)
                         {
-                            Movie1DetailButton.Visible = false;
-                            Movie2DetailButton.Visible = false;
-                            Movie3DetailButton.Visible = false;
-                            Movie4DetailButton.Visible = false;
-                            Movie5DetailButton.Visible = false;
-
-                            BackToHomeButton.Visible = true;
-                            ReviewButton.Visible = false;
+                            
 
                             MovieSearchPage.BringToFront();
 
@@ -314,15 +110,7 @@ namespace DB_Project_Cinema
                         OracleDataReader reader2 = Comm.ExecuteReader();
                         if (reader2.HasRows)
                         {
-                            Movie1DetailButton.Visible = false;
-                            Movie2DetailButton.Visible = false;
-                            Movie3DetailButton.Visible = false;
-                            Movie4DetailButton.Visible = false;
-                            Movie5DetailButton.Visible = false;
-
-                            BackToHomeButton.Visible = true;
-                            ReviewButton.Visible = false;
-
+                           
                             MovieSearchPage.BringToFront();
 
                             while (reader2.Read())
@@ -352,3 +140,5 @@ namespace DB_Project_Cinema
         }
     }
 }
+   
+
